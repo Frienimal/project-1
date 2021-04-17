2a0128210d9f6550dc1de62528e1c3a26a8d4fcd
Parth Cheulkar <pscheulkar12@gmail.com>
2021-04-17 20:00:24 +0530

    r

--- public/app.js
+++ public/app.js
@@ -36,3 +36,23 @@ function signup(){
    form.email.value = '';
  })
 }
+
+
+//Pop-up animation  
+$(document).ready(function(){
+
+    $('.col-4-lg').hover(
+
+        function(){
+            $(this).animate({
+            marginTop: "-=1%",
+        },200);
+        },
+
+        function(){
+            $(this).animate({
+            marginTop: "0%"
+        },200);
+        }
+    );
+});
