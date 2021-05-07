c63faeeb63d06980003aa9cb46963ff39c31bab4
Parth Cheulkar <pscheulkar12@gmail.com>
2021-05-07 17:58:41 +0530

    Merge branch 'main' of https://github.com/Frienimal/project-1

--- public/app.js
+++ public/app.js
@@ -1,18 +1,18 @@
-const PetInfo = document.querySelector('#Pet-info');
-const form2 = document.querySelector('#add-pets-form');
+const PetInfo = document.querySelector("#Pet-info");
+const form2 = document.querySelector("#add-pets-form");
 
-function renderPetInfo(doc){
-  let li = document.createElement('li');
-  let Pet_Name = document.createElement('span');
-  let Pet_Type = document.createElement('span');
-  let vaccination = document.createElement('span');
-  let Pet_Residence= document.createElement('span');
-  let Email = document.createElement('span');
-  let Name = document.createElement('span');
-  let Phone_no = document.createElement('span');
-  let cross = document.createElement('div');
+function renderPetInfo(doc) {
+  let li = document.createElement("li");
+  let Pet_Name = document.createElement("span");
+  let Pet_Type = document.createElement("span");
+  let vaccination = document.createElement("span");
+  let Pet_Residence = document.createElement("span");
+  let Email = document.createElement("span");
+  let Name = document.createElement("span");
+  let Phone_no = document.createElement("span");
+  let cross = document.createElement("div");
 
-  li.setAttribute('data-id', doc.id);
+  li.setAttribute("data-id", doc.id);
   Pet_Name.textContent = doc.data().PetName;
   Pet_Type.textContent = doc.data().PetType;
   vaccination.textContent = doc.data().Vaccination;
@@ -20,7 +20,7 @@ function renderPetInfo(doc){
   Email.textContent = doc.data().Email;
   Name.textContent = doc.data().Name;
   Phone_no.textContent = doc.data().PhoneNo;
-  cross.textContent = 'x';
+  cross.textContent = "x";
 
   li.appendChild(Pet_Name);
   li.appendChild(Pet_Type);
@@ -32,127 +32,102 @@ function renderPetInfo(doc){
   li.appendChild(cross);
 
   //deleting data
-  cross.addEventListener('click', (e) => {
+  cross.addEventListener("click", (e) => {
     e.stopPropagation();
-    let id = e.target.parentElement.getAttribute('data-id');
-    db.collection('PetForm').doc(id).delete();
-  })
-  
-  
-  PetInfo.appendChild(li);
+    let id = e.target.parentElement.getAttribute("data-id");
+    db.collection("PetForm").doc(id).delete();
+  });
 
+  PetInfo.appendChild(li);
 }
 
 // getting data
 //add .where() to for queries
 //orderby() for displaying data in an order
-db.collection('PetForm').get().then((Snapshot) => {
-  Snapshot.docs.forEach(doc =>{
-    renderPetInfo(doc);
-  })
-});
+db.collection("PetForm")
+  .get()
+  .then((Snapshot) => {
+    Snapshot.docs.forEach((doc) => {
+      renderPetInfo(doc);
+    });
+  });
 
 //Saving Data
-form2.addEventListener('submit', (e) => {
+form2.addEventListener("submit", (e) => {
   e.preventDefault();
-  db.collection('PetForm').add({
-      PetName : form2.Petname.value,
-      PetType : form2.Type.value,
-      Vaccination : form2.Vaccination.value,
-      Name : form2.Name.value,
-      Email : form2.Email.value,
-      PhoneNo : form2.PhoneNo.value,
-      PetResidence : form2.Resi.value
+  db.collection("PetForm").add({
+    PetName: form2.Petname.value,
+    PetType: form2.Type.value,
+    Vaccination: form2.Vaccination.value,
+    Name: form2.Name.value,
+    Email: form2.Email.value,
+    PhoneNo: form2.PhoneNo.value,
+    PetResidence: form2.Resi.value,
   });
-  form2.Petname.value = '';
-  form2.Type.value = '';
-  form2.Vaccination.value = '';
-  form2.Name.value = '';
-  form2.Email.value = '';
-  form2.PhoneNo.value = '';
-  form2.Resi.value = '';
-
-})
-
-
+  form2.Petname.value = "";
+  form2.Type.value = "";
+  form2.Vaccination.value = "";
+  form2.Name.value = "";
+  form2.Email.value = "";
+  form2.PhoneNo.value = "";
+  form2.Resi.value = "";
+});
 
 //Google Login
 
-function googlelogin(){
-
+function googlelogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider)
+  firebase
+    .auth()
+    .signInWithPopup(provider)
 
-    .then (result => {
+    .then((result) => {
       const user = result.user;
       window.alert(`Hello ${user.displayName}`);
-      document.querySelector('#A1').textContent = `${user.displayName}`
-      console.log(user)
+      document.querySelector("#A1").textContent = `${user.displayName}`;
+      console.log(user);
     })
-    .catch(console.log)
-
+    .catch(console.log);
 }
 
 //Newsletter SignUp (Saving user Content to Database)
 
- const form = document.querySelector('#add-signup-form');
+const form = document.querySelector("#add-signup-form");
 
- //saving form
-function signup(){
- form.addEventListener('submit',(e)=>{
-   e.preventDefault();
-   db.collection('Email').add({
-      email : form.email.value
-   });
-   form.email.value = '';
- })
+//saving form
+function signup() {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    db.collection("Email").add({
+      email: form.email.value,
+    });
+    form.email.value = "";
+  });
 }
 
-<<<<<<< HEAD
+var firebaseConfig = {
+  apiKey: "AIzaSyCLTQaJIOwSqjmStSlQhl_1YEU0GX3pryw",
+  authDomain: "frienimal.firebaseapp.com",
+  databaseURL: "https://frienimal.firebaseio.com",
+  projectId: "frienimal",
+  storageBucket: "frienimal.appspot.com",
+  messagingSenderId: "264888471735",
+  appId: "1:264888471735:web:b0dce2f4206e31994e3d32",
+  measurementId: "G-4BGZRKHGG6",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 
-//Pop-up animation  
-$(document).ready(function(){
+function signUp() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
 
-    $('.col-4-lg').hover(
+  const promise = auth.createUserWithEmailAndPassword(
+    email.value,
+    password.value
+  );
+  promise.catch((e) => alert(e.message));
 
-        function(){
-            $(this).animate({
-            marginTop: "-=1%",
-        },200);
-        },
-
-        function(){
-            $(this).animate({
-            marginTop: "0%"
-        },200);
-        }
-    );
-});
-=======
-  var firebaseConfig = {
-    apiKey: "AIzaSyCLTQaJIOwSqjmStSlQhl_1YEU0GX3pryw",
-    authDomain: "frienimal.firebaseapp.com",
-    databaseURL: "https://frienimal.firebaseio.com",
-    projectId: "frienimal",
-    storageBucket: "frienimal.appspot.com",
-    messagingSenderId: "264888471735",
-    appId: "1:264888471735:web:b0dce2f4206e31994e3d32",
-    measurementId: "G-4BGZRKHGG6"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-      const auth = firebase.auth();
-
-
-  function signUp(){
-		
-		var email = document.getElementById("email");
-		var password = document.getElementById("password");
-		
-		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-		promise.catch(e => alert(e.message));
-		
-		alert("Signed Up");
-	}
-  
->>>>>>> 57c1968390fd827b97fdfada707623d7add348cd
+  alert("Signed Up");
+}
