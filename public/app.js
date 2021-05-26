9f56cfb94da2a82acfc115c32ff0f0dc9cc010b9
Parth Cheulkar <pscheulkar12@gmail.com>
2021-05-27 00:32:22 +0530

    final changes

--- public/app.js
+++ public/app.js
@@ -10,7 +10,7 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+// const auth = firebase.auth();
 const db = firebase.firestore();
 
 function signUp() {
@@ -26,81 +26,26 @@ var password = document.getElementById("password");
   alert("Signed Up");
 }
 
-const PetInfo = document.querySelector("#Pet-info");
+//add pet
+
 const form2 = document.querySelector("#add-pets-form");
-
-function renderPetInfo(doc) {
-  let li = document.createElement("li");
-  let Pet_Name = document.createElement("span");
-  let Pet_Type = document.createElement("span");
-  let vaccination = document.createElement("span");
-  let Pet_Residence = document.createElement("span");
-  let Email = document.createElement("span");
-  let Name = document.createElement("span");
-  let Phone_no = document.createElement("span");
-  let cross = document.createElement("div");
-
-  li.setAttribute("data-id", doc.id);
-  Pet_Name.textContent = doc.data().PetName;
-  Pet_Type.textContent = doc.data().PetType;
-  vaccination.textContent = doc.data().Vaccination;
-  Pet_Residence.textContent = doc.data().PetResidence;
-  Email.textContent = doc.data().Email;
-  Name.textContent = doc.data().Name;
-  Phone_no.textContent = doc.data().PhoneNo;
-  cross.textContent = "x";
-
-  li.appendChild(Pet_Name);
-  li.appendChild(Pet_Type);
-  li.appendChild(vaccination);
-  li.appendChild(Pet_Residence);
-  li.appendChild(Email);
-  li.appendChild(Name);
-  li.appendChild(Phone_no);
-  
-
-  //deleting data
-  // cross.addEventListener("click", (e) => {
-  //   e.stopPropagation();
-  //   let id = e.target.parentElement.getAttribute("data-id");
-  //   db.collection("PetForm").doc(id).delete();
-  // });
-
-  PetInfo.appendChild(li);
-}
-
-// getting data
-//add .where() to for queries
-//orderby() for displaying data in an order
-db.collection("PetForm").get().then((Snapshot) => {
-    Snapshot.docs.forEach((doc) => {
-      renderPetInfo(doc);
-    });
-  });
-
-//Saving Data
-function uploadImage(){
-  const ref = firebase.storage().ref()
-  const file = document.querySelector("#photo").files[0]
-  const name = new Date() + '-' + file.name
-  const metadata = {
-      contentType:file.type
-  }
- 
-  const task = ref.child(name).put(file,metadata)
-
-  task
-  .then(snapshot => snapshot.ref.getDownloadURL())
-  .then(url => {
-      console.log(url)
-      alert("Image uploaded")
-      const image = document.querySelector('#image')
-      image.src = url
-  })
-}
-
 form2.addEventListener("submit", (e) => {
-  e.preventDefault();
+  // e.preventDefault();
+  console.log("Added");
+  // const ref = firebase.storage().ref()
+  // const file = document.querySelector("#photo").files[0]
+  // const name = new Date() + '-' + file.name
+  // const metadata = {
+  //     contentType:file.type
+  // }
+  //   const task = ref.child(name).put(file,metadata)
+  //   task
+  //   .then(snapshot => snapshot.ref.getDownloadURL())
+  //   .then(url => {
+  //       const image = document.querySelector('#image')
+  //       image.src = url
+  //   })
+  
   db.collection("PetForm").add({
     PetName: form2.Petname.value,
     PetType: form2.Type.value,
@@ -109,14 +54,100 @@ form2.addEventListener("submit", (e) => {
     Email: form2.Email.value,
     PhoneNo: form2.PhoneNo.value,
     PetResidence: form2.Resi.value,
-    Image:form2.Image.value
+    PetImage:form2.Image.value,
   });
-  form2.Petname.value = '';
-  form2.Type.value = '';
-  form2.Vaccination.value = '';
-  form2.Name.value = '';
-  form2.Email.value = '';
-  form2.PhoneNo.value = '';
-  form2.Resi.value = '';
+  console.log('Completed');
+  window.location.href = "https://www.frienimal.web.app/pets.html";
+  // form2.Petname.value = '';
+  // form2.Type.value = '';
+  // form2.Vaccination.value = '';
+  // form2.Name.value = '';
+  // form2.Email.value = '';
+  // form2.PhoneNo.value = '';
+  // form2.Resi.value = '';
+  // form2.Image.value = image.src;
 });
+
+// firebase.initializeApp(firebaseConfig);
+      
+// const db = firebase.firestore();
+// db.settings({timestampInSnapshots: true});
+
+// function uploadImage(){
+// const ref = firebase.storage().ref()
+
+// const file = document.querySelector("#photo").files[0]
+
+// const name = new Date() + '-' + file.name
+
+// const metadata = {
+//     contentType:file.type
+// }
+
+// const task = ref.child(name).put(file,metadata)
+
+// task
+// .then(snapshot => snapshot.ref.getDownloadURL())
+// .then(url => {
+//     console.log(url)
+//     alert("Image uploaded")
+//   }
+// )
+// }
+
+// Pet Forms
+
+const PetInfo = document.querySelector("#Pet-info");
+
+
+// function renderPetInfo(doc) {
+//   let li = document.createElement("li");
+//   let Pet_Name = document.createElement("span");
+//   let Pet_Type = document.createElement("span");
+//   let vaccination = document.createElement("span");
+//   let Pet_Residence = document.createElement("span");
+//   let Email = document.createElement("span");
+//   let Name = document.createElement("span");
+//   let Phone_no = document.createElement("span");
+//   let cross = document.createElement("div");
+
+//   li.setAttribute("data-id", doc.id);
+//   Pet_Name.textContent = doc.data().PetName;
+//   Pet_Type.textContent = doc.data().PetType;
+//   vaccination.textContent = doc.data().Vaccination;
+//   Pet_Residence.textContent = doc.data().PetResidence;
+//   Email.textContent = doc.data().Email;
+//   Name.textContent = doc.data().Name;
+//   Phone_no.textContent = doc.data().PhoneNo;
+//   cross.textContent = "x";
+
+//   li.appendChild(Pet_Name);
+//   li.appendChild(Pet_Type);
+//   li.appendChild(vaccination);
+//   li.appendChild(Pet_Residence);
+//   li.appendChild(Email);
+//   li.appendChild(Name);
+//   li.appendChild(Phone_no);
+  
+
+//   //deleting data
+//   // cross.addEventListener("click", (e) => {
+//   //   e.stopPropagation();
+//   //   let id = e.target.parentElement.getAttribute("data-id");
+//   //   db.collection("PetForm").doc(id).delete();
+//   // });
+
+//   PetInfo.appendChild(li);
+// }
+
+// // getting data
+// //add .where() to for queries
+// //orderby() for displaying data in an order
+// db.collection("PetForm").get().then((Snapshot) => {
+//     Snapshot.docs.forEach((doc) => {
+//       renderPetInfo(doc);
+//     });
+//   });
+
+//Saving Data
 
